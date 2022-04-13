316eb5d322ed532a3c759592d13a2e008c539f23
anafedechem <anafedechem@gmail.com>
2022-04-13 17:20:38 -0300

    adicionei lógica no js para colocação de imagem e descrição de imagem conforme a data escolhida

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-var requisicao = new XMLHttpRequest(); //passo 1: cria a requisição
-
-requisicao.open("GET", "https://api.nasa.gov/planetary/apod"); //passo 2: configura a requisição
-
-requisicao.onload = function () { //ajax/onload é antes do send
-    if (requisicao.status == "DATA ESCOLHIDA PELA PESSOA, PEGAR O VALOR DO CALENDARIO") {
-        let response = requisicao.response;
-
-
-    }
-}
-
-
 //--------------------------------------------------------------------------------------
 
 $('#submit').click(event => {
@@ -19,11 +6,14 @@ $('#submit').click(event => {
     let chaveAPI = `odGcc4PNIa8SDI7K4nf0wrjzCTUxLBqe3UwMlSj7`
     $.ajax({
         url: `https://api.nasa.gov/planetary/apod?date=${data}&api_key=${chaveAPI}`,
+
         success: function (response) {
             let url = response.url
-            document.getElementById("img-do-dia").src = url;
-           //inserir imagem da data correspondente fornecida no input data-selecionada
-          //
+            let description = response.title
+
+            $("#img-do-dia").attr("src", url);
+            $("#descricao").text(description);
+
         }
     })
 });
