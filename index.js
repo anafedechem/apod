60481ab4792619ab78e301dd9187c1d9d3558b0a
anafedechem <anafedechem@gmail.com>
2022-04-13 19:35:54 -0300

    finalizei lógica de javascript

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
 //--------------------------------------------------------------------------------------
 
+
 $('#submit').click(event => {
     event.preventDefault();
-    let data = $('#data-selecionada').val()
+    let dataSelecionadaTexto = $('#data-selecionada').val()
     let chaveAPI = `odGcc4PNIa8SDI7K4nf0wrjzCTUxLBqe3UwMlSj7`
+    let dataDeHoje = new Date();
+
+    if (dataSelecionadaTexto == "") {
+        alert("Escolha uma data");
+        return
+    }
+
+    let dataSelecionada = new Date(dataSelecionadaTexto)
+
+    if (dataSelecionada > dataDeHoje) {
+        alert("Esolha uma data válida")
+        return
+    }
+
     $.ajax({
-        url: `https://api.nasa.gov/planetary/apod?date=${data}&api_key=${chaveAPI}`,
-
+        url: `https://api.nasa.gov/planetary/apod?date=${dataSelecionadaTexto}&api_key=${chaveAPI}`,
         success: function (response) {
-            let url = response.url
-            let description = response.title
+            let url = response.url;
+            let description = response.title;
+            let mediaType = response.media_type;
 
-            $("#img-do-dia").attr("src", url);
-            $("#descricao").text(description);
-
+            let imgDoDia = $('#img-do-dia');
+            let videoDoDia = $('#video-do-dia');
+            let descricao = $("#descricao");  
+            
+            descricao.text(description);
+            if (mediaType == "image") {
+                imgDoDia.attr("src", url);
+                imgDoDia.show();
+                videoDoDia.hide();
+            } else {
+                videoDoDia.attr("src", url);
+                videoDoDia.show();
+                imgDoDia.hide();
+            }
         }
-    })
+    });
 });
